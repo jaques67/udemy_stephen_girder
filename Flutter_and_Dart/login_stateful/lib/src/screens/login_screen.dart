99c35a7bb6a2d3e650bbc9c8046958810b36a6b6
Jaques Strydom <jaques@cyberia.technology>
2024-02-20 14:24:05 +0200

    Lesson 107
    Creating the Login Screen
    Added StatefulWidget boiler plate

---
+++ Flutter_and_Dart/login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
