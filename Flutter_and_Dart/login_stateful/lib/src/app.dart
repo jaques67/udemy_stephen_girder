9ba1663b3ec5d0cffeda9ae160c0570064456138
Jaques Strydom <jaques@cyberia.technology>
2024-02-20 13:35:48 +0200

    Lesson 106
    Boilerplate App Code

---
+++ Flutter_and_Dart/login_stateful/lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return const MaterialApp(
+      title: 'Log me in!',
+      home: Scaffold(
+        body: Text('Show a form here!'),
+      ),
+    );
+  }
+}
