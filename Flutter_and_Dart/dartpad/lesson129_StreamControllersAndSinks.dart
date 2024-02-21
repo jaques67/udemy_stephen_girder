9be4d6a6ef55ab70763d0d24611c404e3e7313c1
Jaques Strydom <jaques@cyberia.technology>
2024-02-21 14:45:11 +0200

    Lesson 127, 128 and 129
    Streams by Analogy
    Characteristics of Streams
    StreamControllers and Sinks

---
+++ Flutter_and_Dart/dartpad/lesson129_StreamControllersAndSinks.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+class Cake {}
+
+class Order {
+  String type;
+  Order(this.type);
+}
+
+void main() {
+  final controller = new StreamController();
+
+  final order = new Order('banana');
+
+  controller.sink.add(order);
+}
