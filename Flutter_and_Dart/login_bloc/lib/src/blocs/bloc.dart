3588c2cb581b88e1139f7720fae3a923538cac09
Jaques Strydom <jaques@cyberia.technology>
2024-02-27 12:23:53 +0200

    Lesson 156
    Cleaning Up Controllers
    Previously Dart required that we eventually close the sinks associated
    with our controllers. It no longer seems to be necessary.

--- Flutter_and_Dart/login_bloc/lib/src/blocs/bloc.dart
+++ Flutter_and_Dart/login_bloc/lib/src/blocs/bloc.dart
@@ -6,10 +6,16 @@ class Bloc with Validators {
   final _password = StreamController<String>();
 
   // Add data to stream
-  Stream<String> get email => _email.stream;
-  Stream<String> get password => _password.stream;
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
 
   // Change data
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
+
+  // Don't think this is required anymore.
+  dispose() {
+    _email.close();
+    _password.close();
+  }
 }
