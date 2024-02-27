93d64f2cae066f0f090a2f4553349d27bd8f9f60
Jaques Strydom <jaques@cyberia.technology>
2024-02-27 07:34:32 +0200

    Lesson 146
    TextFields with RaisedButtons
    Used ElevatedButton as RaisedButton has been deprecated.
    Added style to button as color is no longer available.

--- Flutter_and_Dart/login_bloc/lib/src/screens/login_screen.dart
+++ Flutter_and_Dart/login_bloc/lib/src/screens/login_screen.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
 
-
 class LoginScreen extends StatelessWidget {
   Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: [
-
+          emailField(),
+          passwordField(),
+          Container(
+            margin: const EdgeInsets.only(top: 25.0),
+          ),
+          submitButton(),
         ],
       ),
+    );
+  }
+
+  Widget emailField() {
+    return const TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return const TextField(
+      obscureText: false,
+      decoration: InputDecoration(
+        hintText: 'password',
+        labelText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(backgroundColor: Colors.blue);
+    return ElevatedButton(
+      onPressed: () {},
+      style: style,
+      child: const Text('Login'),
     );
   }
 }
