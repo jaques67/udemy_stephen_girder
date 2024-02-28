98de14e1927ef7430c26aa0575b97ecf0e86f9e2
Jaques Strydom <jaques@cyberia.technology>
2024-02-28 14:38:32 +0200

    Lesson 187
    Tweens with Curves.
    The rate at which the animated value changes.

--- Flutter_and_Dart/animation/lib/src/screens/home.dart
+++ Flutter_and_Dart/animation/lib/src/screens/home.dart
@@ -18,6 +18,13 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       duration: const Duration(seconds: 2),
       vsync: this,
     );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn,
+      ),
+    );
   }
 
   @override
