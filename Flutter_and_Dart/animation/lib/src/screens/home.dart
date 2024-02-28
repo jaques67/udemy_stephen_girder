ee3e49f487965cc257bc584855b9a9e1abfe23ba
Jaques Strydom <jaques@cyberia.technology>
2024-02-28 13:32:20 +0200

    Lesson 184
    Displaying a Cat
    Code now runs and displays a cat on the screen.

--- Flutter_and_Dart/animation/lib/src/screens/home.dart
+++ Flutter_and_Dart/animation/lib/src/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -17,6 +18,6 @@ class HomeState extends State<Home> {
   }
 
   Widget buildAnimation() {
-
+    return Cat();
   }
 }
