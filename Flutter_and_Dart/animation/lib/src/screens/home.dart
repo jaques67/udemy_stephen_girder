0e1531f36b080c6dbaba77fb5dde9700f9dd9788
Jaques Strydom <jaques@cyberia.technology>
2024-02-29 15:00:37 +0200

    Lesson 201
    Negative Offsets
    Cat is still being clipped by the Stack viewport.

--- Flutter_and_Dart/animation/lib/src/screens/home.dart
+++ Flutter_and_Dart/animation/lib/src/screens/home.dart
@@ -19,7 +19,7 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       vsync: this,
     );
 
-    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+    catAnimation = Tween(begin: -50.0, end: 100.0).animate(
       CurvedAnimation(
         parent: catController,
         curve: Curves.easeIn,
@@ -60,7 +60,7 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       animation: catAnimation,
       builder: (context, child) {
         return Positioned(
-          bottom: catAnimation.value,
+          top: catAnimation.value,
           left: 0.0,
           right: 0.0,
           child: child!,
