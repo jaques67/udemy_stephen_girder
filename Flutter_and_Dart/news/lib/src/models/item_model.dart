81e9887a39a1b20334f542733d026d4046cf952c
Jaques Strydom <jaques@cyberia.technology>
2024-03-04 14:52:58 +0200

    Lesson 222
    A Few More ItemModel Properties

--- Flutter_and_Dart/news/lib/src/models/item_model.dart
+++ Flutter_and_Dart/news/lib/src/models/item_model.dart
@@ -12,4 +12,19 @@ class ItemModel {
   final int score;
   final String title;
   final int descendants;
+
+  ItemModel.fromJSON(Map<String, dynamic> parsedJSon)
+    : id = parsedJSon['id'],
+      deleted = parsedJSon['deleted'],
+      type = parsedJSon['type'],
+      by = parsedJSon['by'],
+      time = parsedJSon['time'],
+      text = parsedJSon['text'],
+      dead = parsedJSon['dead'],
+      parent = parsedJSon['parent'],
+      kids = parsedJSon['kids'],
+      url = parsedJSon['url'],
+      score = parsedJSon['score'],
+      title = parsedJSon['title'],
+      descendants = parsedJSon['descendants'];
 }
