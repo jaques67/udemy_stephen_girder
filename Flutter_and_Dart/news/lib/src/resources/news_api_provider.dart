5fa5e6e12a9e278aa5ce95288fcfe8d2887999b3
Jaques Strydom <jaques@cyberia.technology>
2024-03-04 15:20:22 +0200

    Lesson 223
    API Provider Implementation
    Added http to pubspec
    The URL string has to be cast as a Uri to be used.

---
+++ Flutter_and_Dart/news/lib/src/resources/news_api_provider.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart' show Client;
+
+
+class NewsApiProvider {
+  Client client = Client();
+
+  fetchTopIds() {
+    client.get('https://hacker-news.firebaseio.com/v0/topstories.json' as Uri);
+  }
+
+  fetchItem() {
+
+  }
+}
