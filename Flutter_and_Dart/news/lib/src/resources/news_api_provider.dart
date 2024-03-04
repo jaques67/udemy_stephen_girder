40bf41707fc147885b16827bcd0cd3b730b12c17
Jaques Strydom <jaques@cyberia.technology>
2024-03-04 15:49:48 +0200

    Lesson 224
    Fetching Individual Items
    Returns the parsed Json of the item in an instance of ItemModel.

--- Flutter_and_Dart/news/lib/src/resources/news_api_provider.dart
+++ Flutter_and_Dart/news/lib/src/resources/news_api_provider.dart
@@ -1,14 +1,25 @@
+import 'dart:convert';
 import 'package:http/http.dart' show Client;
+import '../models/item_model.dart';
 
+const _root = 'https://hacker-news.firebaseio.com/v0';
 
 class NewsApiProvider {
   Client client = Client();
 
-  fetchTopIds() {
-    client.get('https://hacker-news.firebaseio.com/v0/topstories.json' as Uri);
+  fetchTopIds() async {
+    final response = await client
+        .get('$_root/topstories.json' as Uri);
+    final ids = jsonDecode(response.body);
+
+    return ids;
   }
 
-  fetchItem() {
+  fetchItem(int id) async {
+    final response = await client
+        .get('$_root/item/$id.json' as Uri);
+    final parsedJson = jsonDecode(response.body);
 
+    return ItemModel.fromJSON(parsedJson);
   }
 }
