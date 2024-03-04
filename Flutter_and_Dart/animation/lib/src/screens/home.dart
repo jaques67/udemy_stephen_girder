c08894f391865d933f7c0b32bf8408c6875a3ce8
Jaques Strydom <jaques@cyberia.technology>
2024-03-04 10:19:37 +0200

    Lesson 208
    A Touch of Positioning
    Moved the left flap slightly

--- Flutter_and_Dart/animation/lib/src/screens/home.dart
+++ Flutter_and_Dart/animation/lib/src/screens/home.dart
@@ -82,13 +82,16 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
   }
 
   Widget buildLeftFlap() {
-    return Transform.rotate(
-      angle: pi / 2.0,
-      alignment: Alignment.topLeft,
-      child: Container(
-        height: 10.0,
-        width: 125.0,
-        color: Colors.red,
+    return Positioned(
+      left: 3.0,
+      child: Transform.rotate(
+        angle: pi * 0.6,
+        alignment: Alignment.topLeft,
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown,
+        ),
       ),
     );
   }
