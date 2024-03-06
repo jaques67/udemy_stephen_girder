9182261b8d184adbb4bbccefd0af66fe3f7a2314
Jaques Strydom <jaques@cyberia.technology>
2024-03-06 11:12:23 +0200

    Lesson 227
    Writing Expectations
    First test was a really simple test.

--- Flutter_and_Dart/news/test/src/resources/news_api_provider_test.dart
+++ Flutter_and_Dart/news/test/src/resources/news_api_provider_test.dart
@@ -4,3 +4,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
 
+void main() {
+  test('FetchTopIds returns a list of ids', () {
+    // Setup of test case
+    const sum = 1 + 3;
+
+    // expectation
+    expect(sum, 4);
+  });
+}
