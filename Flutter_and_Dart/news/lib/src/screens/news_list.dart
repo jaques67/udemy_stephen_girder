cca336ef8ec897aee89826efa082be33468aff40
Jaques Strydom <jaques.strydom@gmail.com>
2024-03-12 11:22:16 +0200

    Removed temp code

--- Flutter_and_Dart/news/lib/src/screens/news_list.dart
+++ Flutter_and_Dart/news/lib/src/screens/news_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
@@ -7,33 +6,7 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: BuildList(),
-    );
-  }
-
-  Widget BuildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            return Container(
-              height: 80,
-              child: snapshot.hasData
-                  ? Text('Im visible $index')
-                  : Text('I havent fetched data yet $index'),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'hi',
+      body: Text('Show a List'),
     );
   }
 }
