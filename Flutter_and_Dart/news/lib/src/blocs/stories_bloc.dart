83aef2640de7f757fe4290eb701e5bccea505e91
Jaques Strydom <jaques.strydom@gmail.com>
2024-03-12 14:42:18 +0200

    Lesson 265
    Exposing Bloc Getters

--- Flutter_and_Dart/news/lib/src/blocs/stories_bloc.dart
+++ Flutter_and_Dart/news/lib/src/blocs/stories_bloc.dart
@@ -3,10 +3,16 @@ import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class StoriesBloc {
+  final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
 
   // Getters to Streams
   Stream<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopIds();
+    _topIds.sink.add(ids);
+  }
 
   // No longer required
   // dispose(){
