aa35479e96ad8d103bf761b3abb5d2fdf92e384d
Jaques Strydom <jaques.strydom@gmail.com>
2024-03-13 11:36:26 +0200

    Lesson 268
    Type Annotations Solve Problems
    Used string interpolation instead of toString() to convert int value for
    display purposes.

--- Flutter_and_Dart/news/lib/src/screens/news_list.dart
+++ Flutter_and_Dart/news/lib/src/screens/news_list.dart
@@ -21,7 +21,7 @@ class NewsList extends StatelessWidget {
   Widget buildList(StoriesBloc bloc) {
     return StreamBuilder(
       stream: bloc.topIds,
-      builder: (context, snapshot) {
+      builder: (context, AsyncSnapshot<List<int>> snapshot) {
         if (!snapshot.hasData) {
           return Text('Still waiting on IDs');
         }
@@ -29,7 +29,8 @@ class NewsList extends StatelessWidget {
         return ListView.builder(
           itemCount: snapshot.data?.length,
           itemBuilder: (context, int index) {
-            return Text(snapshot.data?[index].toString() ?? 'Loading...');
+            // return Text(snapshot.data?[index].toString() ?? 'Loading...');
+            return Text('${snapshot.data?[index]}');
           },
         );
       },
